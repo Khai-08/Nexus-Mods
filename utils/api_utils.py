2b69d70055af1d7dc98da74fcfc1837b3f842fc1
Khai-08 <khalil.aparecio0308@gmail.com>
2025-05-01 22:01:59 +0800

    Update api_utils.py
    Added fetch_all_games()

--- utils/api_utils.py
+++ utils/api_utils.py
@@ -47,3 +47,15 @@ async def fetch_game(game: str, headers: dict):
     except Exception as e:
         print(f"Error fetching mods: {e}")
     return None
+
+async def fetch_all_games(headers: dict):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://api.nexusmods.com/v1/games.json", headers=headers) as response:
+                if response.status == 200:
+                    return await response.json()
+                else:
+                    return None
+    except Exception as e:
+        print(f"Error fetching mods: {e}")
+    return None
