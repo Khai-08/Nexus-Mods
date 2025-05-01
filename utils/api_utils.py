5bc18ceeca82987026917c5e2c2b6d2e09262dd1
Khai-08 <khalil.aparecio0308@gmail.com>
2025-05-01 20:37:42 +0800

    Create api_utils.py

---
+++ utils/api_utils.py
@@ -0,0 +1,37 @@
+import aiohttp
+
+async def fetch_latest_mods(game: str, headers: dict):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://api.nexusmods.com/v1/games/{game}/mods/latest_added.json", headers=headers) as response:
+                if response.status == 200:
+                    return await response.json()
+                else:
+                    return None
+    except Exception as e:
+        print(f"Error fetching mods: {e}")
+    return None
+
+async def fetch_updated_mods(game: str, headers: dict):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://api.nexusmods.com/v1/games/{game}/mods/latest_updated.json", headers=headers) as response:
+                if response.status == 200:
+                    return await response.json()
+                else:
+                    return None
+    except Exception as e:
+        print(f"Error fetching mods: {e}")
+    return None
+
+async def fetch_trending_mods(game: str, headers: dict):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://api.nexusmods.com/v1/games/{game}/mods/trending.json", headers=headers) as response:
+                if response.status == 200:
+                    return await response.json()
+                else:
+                    return None
+    except Exception as e:
+        print(f"Error fetching mods: {e}")
+    return None
