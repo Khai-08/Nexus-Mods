1d0c3ef6a4eb00565dfa40e5f50014492d5b6d48
Khai-08 <khalil.aparecio0308@gmail.com>
2025-05-02 17:52:03 +0800

    Update api_utils.py

--- utils/api_utils.py
+++ utils/api_utils.py
@@ -1,9 +1,13 @@
-import aiohttp
+import os, aiohttp
+from dotenv import load_dotenv
 
-async def fetch_latest_mods(game: str, headers: dict):
+load_dotenv()
+BASE_URL = os.getenv("API_BASE_URL")
+
+async def fetch_mod_by_game(game_domain_name: str, mod_id: int, headers: dict):
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api.nexusmods.com/v1/games/{game}/mods/latest_added.json", headers=headers) as response:
+            async with session.get(f"{BASE_URL}/v1/games/{game_domain_name}/mods/{mod_id}.json", headers=headers) as response:
                 if response.status == 200:
                     return await response.json()
                 else:
@@ -12,10 +16,10 @@ async def fetch_latest_mods(game: str, headers: dict):
         print(f"Error fetching mods: {e}")
     return None
 
-async def fetch_updated_mods(game: str, headers: dict):
+async def fetch_latest_mods(game_domain_name: str, headers: dict):
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api.nexusmods.com/v1/games/{game}/mods/latest_updated.json", headers=headers) as response:
+            async with session.get(f"{BASE_URL}/v1/games/{game_domain_name}/mods/latest_added.json", headers=headers) as response:
                 if response.status == 200:
                     return await response.json()
                 else:
@@ -24,10 +28,10 @@ async def fetch_updated_mods(game: str, headers: dict):
         print(f"Error fetching mods: {e}")
     return None
 
-async def fetch_trending_mods(game: str, headers: dict):
+async def fetch_updated_mods(game_domain_name: str, headers: dict):
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api.nexusmods.com/v1/games/{game}/mods/trending.json", headers=headers) as response:
+            async with session.get(f"{BASE_URL}/v1/games/{game_domain_name}/mods/latest_updated.json", headers=headers) as response:
                 if response.status == 200:
                     return await response.json()
                 else:
@@ -36,10 +40,22 @@ async def fetch_trending_mods(game: str, headers: dict):
         print(f"Error fetching mods: {e}")
     return None
 
-async def fetch_game(game: str, headers: dict):
+async def fetch_trending_mods(game_domain_name: str, headers: dict):
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api.nexusmods.com/v1/games/{game}.json", headers=headers) as response:
+            async with session.get(f"{BASE_URL}/v1/games/{game_domain_name}/mods/trending.json", headers=headers) as response:
+                if response.status == 200:
+                    return await response.json()
+                else:
+                    return None
+    except Exception as e:
+        print(f"Error fetching mods: {e}")
+    return None
+
+async def fetch_game(game_domain_name: str, headers: dict):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{BASE_URL}/v1/games/{game_domain_name}.json", headers=headers) as response:
                 if response.status == 200:
                     return await response.json()
                 else:
@@ -51,7 +67,7 @@ async def fetch_game(game: str, headers: dict):
 async def fetch_all_games(headers: dict):
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api.nexusmods.com/v1/games.json", headers=headers) as response:
+            async with session.get(f"{BASE_URL}/v1/games.json", headers=headers) as response:
                 if response.status == 200:
                     return await response.json()
                 else:
